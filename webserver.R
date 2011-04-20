86f6b83352162219efd2af76f1ef7f91d43cceee
Ferdinand Jamitzky <f.jamitzky@gmail.com>
2011-04-20 01:29:16 -0700

    Edited webserver.R via GitHub

--- webserver.R
+++ webserver.R
@@ -5,14 +5,14 @@ http.serve=function(){
         gg=unlist(strapply(read.socket(ss),"GET /(.*?) HTTP",c))[[1]]
         res=eval.parent(parse(text=gg))
         reply=
-"HTTP/1.1 200 OK
+gsubfn(,,"HTTP/1.1 200 OK
 Server: R 2.8.1
 Content-Length: `nchar(res)`
 Content-Type: text/html
 Connection: close
 
-"
-        write.socket(ss,paste(reply,res,sep=""))
+`res`")
+        write.socket(ss,reply)
         close.socket(ss)
         }
 http.serve()
